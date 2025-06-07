731c5eee1a8f5771f9d1a068b7733331b09751cf
isha6123 <124663017+isha6123@users.noreply.github.com>
2025-06-07 13:32:33 +0530

    Delete models directory

--- models/Stock.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const stockSchema = new mongoose.Schema({
-  quantity: { type: Number, required: true },
-  location: { type: String }
-});
-
-module.exports = mongoose.model('Stock', stockSchema);
